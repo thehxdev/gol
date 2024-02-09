24c967b64d0262fa82ceed1db800685cc0c72cbb
thehxdev <xclimax1324@gmail.com>
2024-02-09 17:25:09 +0330

    fixed package name in nix flake

--- flake.nix
+++ flake.nix
@@ -1,12 +1,12 @@
 {
-    description = "GOL Nix Flake";
+    description = "GoL Nix Flake";
 
     inputs = {
         nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     };
 
     outputs = { self, nixpkgs, ... }: let
-        pkgName = "GOL";
+        pkgName = "gol";
 
         supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
 
